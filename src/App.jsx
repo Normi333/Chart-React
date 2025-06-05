e14a9b00c7a80106d401be0d289b7471554775a7
Normi333 <shisirthapa146@gmail.com>
2025-06-05 12:22:07 +0545

    added NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChartGrid from "./utils/ChartGrid";
+import NavBar from "./components/NavBar";
 import "./styles/App.css";
 
 function App() {
   return (
-    <>
+    <Router>
+      <NavBar />
       <div className="App">
-        <ChartGrid />
+        <Routes>
+          <Route path="/" element={<ChartGrid />} />
+          {/* <Route path="/map" element={<MapPage />} /> */}
+          {/* <Route path="/report" element={<ReportPage />} /> */}
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/NavBar.css";
 
-function NavBar() {
+const NavBar = () => {
   return (
-    <div>
-      <p>NavBar</p>
-    </div>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <h1 className="navbar-title">Dashboard</h1>
+        <ul className="navbar-links">
+          <li>
+            <NavLink
+              to="/map"
+              className="nav-link"
+              activeclassname="active-link"
+            >
+              Map
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/report"
+              className="nav-link"
+              activeclassname="active-link"
+            >
+              Report
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
-}
+};
 
 export default NavBar;
